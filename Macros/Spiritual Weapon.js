addf66606c6859c8b5725b550be32844803d13df
HavocsCall <49051014+HavocsCall@users.noreply.github.com>
2022-06-17 15:05:10 -0400

    Update Spiritual Weapon.js

--- Macros/Spiritual Weapon.js
+++ Macros/Spiritual Weapon.js
@@ -1,29 +1,31 @@
-//Data from Args
-const actorID = args[1]; // Gets actor ID
-const tokenID = args[2]; // Gets Token ID
-const summonerDC = actorID.data.data.attributes.spelldc;
-const summonerAttack = summonerDC - 8;
-const summonerMod = getProperty(tokenID.actor, `data.data.abilities.${getProperty(tokenID.actor, 'data.data.attributes.spellcasting')}.mod`);
+const actorID = args[2].actorId;
+const spellDC = game.actors.get(actorID).data.data.attributes.spelldc;
+const spellAttack = spellDC - 8;
+const spellAbility = game.actors.get(actorID).data.data.attributes.spellcasting;
+const spellMod = game.actors.get(actorID).data.data.abilities[spellAbility].mod;
+const tokenID = args[2].tokenId;
+const tokenName = canvas.tokens.get(tokenID).data.name;
 
-const level = args[3] - 1 //Gets spell level and + or - to equal amount of dice
+//Set amount of dice here
+const dice = Math.floor(args[1]/2);
 
 //Warpgate Variables
 const summonType = "Spiritual Weapon";
 const updates = {
 	actor: {
-		name:`${summonType} of ${tokenID.name}`,
+		name:`${summonType} of ${tokenName}`,
 	},
 	token: {
-		name:`${summonType} of ${tokenID.name}`,
+		name:`${summonType} of ${tokenName}`,
 	},
 	embedded: {
 		Item: {
 			"Spiritual Weapon Attack": { //String
-				type: "feat", // feat, spell
-				img: "icons/skills/melee/strike-blade-knife-blue-red.webp", //Example: "icons/magic/holy/projectiles-blades-salvo-yellow.webp"
+				type: "feat", // feat, spell, weapon
+				img: "icons/skills/melee/strike-blade-knife-blue-red.webp", // Example: "icons/magic/holy/projectiles-blades-salvo-yellow.webp"
 				data: {
 					description: {
-						value: "You can make a melee spell attack against a creature within 5 feet of the weapon. On a hit, the target takes force damage equal to 1d8 + your spellcasting ability modifier.", //String
+						value: "You can make a melee spell attack against a creature within 5 feet of the weapon. On a hit, the target takes force damage equal to 1d8 + your spellcasting ability modifier.<br>When you cast this spell using a spell slot of 3rd level or higher, the damage increases by 1d8 for every two slot levels above 2nd.", // String
 					},
 					activation: {
 						type: "bonus", // action, bonus, crew, day, hour, lair, legendary, minute, none, reaction, special, reactiondamage, reactionmanual
@@ -38,10 +40,10 @@ const updates = {
 						units: "ft" // self, touch, spec, any, ft, mi, m, km
 					},
 					actionType: "msak", // mwak, rwak, msak, rsak, save, heal, abil, util, other
-					attackBonus: `- @mod - @prof + ${summonerAttack}`, // Formula
+					attackBonus: `- @mod - @prof + ${spellAttack}`, // Formula
 					damage: {
 						parts: [
-						[`${level}d8 + ${summonerMod}`, "force"] // ["Formula", "acid, bludgeoning, cold, fire, force, lightning, necrotic, piercing, poison, psychic, radiant, slashing, thunder, healing, temphp"]
+						[`${dice}d8 + ${spellMod}`, "force"] // ["Formula: this can be like '${dice}d8' (include the backticks) to use the spell level", "acid, bludgeoning, cold, fire, force, lightning, necrotic, piercing, poison, psychic, radiant, slashing, thunder, healing, temphp"]
 						],
 					},
 				}
@@ -56,5 +58,5 @@ if(args[0] === "on") {
 	await warpgate.spawn(summonType, updates, callbacks, options);
 }
 else if(args[0] === "off") {
-	await warpgate.dismiss(summonType);
+    await warpgate.dismiss(); //Still have to figure this one out
 }
