b23b8cd008fe3b62089e65e5634ab54f8236fd73
HavocsCall <49051014+HavocsCall@users.noreply.github.com>
2022-06-18 11:58:00 -0400

    Update Spiritual Weapon.js

--- Macros/Spiritual Weapon.js
+++ Macros/Spiritual Weapon.js
@@ -1,26 +1,26 @@
-const actorID = args[2].actorId;
-const spellDC = game.actors.get(actorID).data.data.attributes.spelldc;
-const spellAttack = spellDC - 8;
-const spellAbility = game.actors.get(actorID).data.data.attributes.spellcasting;
-const spellMod = game.actors.get(actorID).data.data.abilities[spellAbility].mod;
-const tokenID = args[2].tokenId;
-const tokenName = canvas.tokens.get(tokenID).data.name;
+const actorID = args[2].actorId; // Gets casting actor's ID
+const spellDC = game.actors.get(actorID).data.data.attributes.spelldc; // Gets casting actor's spell DC
+const spellAttack = spellDC - 8; // Gets casting actor's spell attack
+const spellAbility = game.actors.get(actorID).data.data.attributes.spellcasting; // Gets casting actor's spellcasting ability
+const spellMod = game.actors.get(actorID).data.data.abilities[spellAbility].mod; // Gets casting actor's spellcasting ability mod
+const tokenID = args[2].tokenId; // Gets casting token's ID
+const tokenName = canvas.tokens.get(tokenID).data.name; // Gets casting token's name
 
 //Set amount of dice here
-const dice = Math.floor(args[1]/2);
+const dice = Math.floor(args[1]/2); // Gets spell level and allows for any math related to spell level to dice conversion
 
 //Warpgate Variables
-const summonType = "Spiritual Weapon";
-const updates = {
-	actor: {
-		name:`${summonType} of ${tokenName}`,
+const summonType = "Spiritual Weapon"; // Make this the same as the name of the actor
+const updates = { // Updates to token on summon
+	actor: { // Need to get the rest of the usual fields for actor
+		name:`${summonType} of ${tokenName}`, // Change the name of the token's actor
 	},
-	token: {
-		name:`${summonType} of ${tokenName}`,
+	token: { // Need to get the rest of the usual fields for token
+		name:`${summonType} of ${tokenName}`, // Change the name of the token
 	},
 	embedded: {
-		Item: {
-			"Spiritual Weapon Attack": { //String
+		Item: { // Define the item that you want in the summoned token's inventory
+			"Spiritual Weapon Attack": { // String
 				type: "feat", // feat, spell, weapon
 				img: "icons/skills/melee/strike-blade-knife-blue-red.webp", // Example: "icons/magic/holy/projectiles-blades-salvo-yellow.webp"
 				data: {
@@ -43,7 +43,7 @@ const updates = {
 					attackBonus: `- @mod - @prof + ${spellAttack}`, // Formula
 					damage: {
 						parts: [
-						[`${dice}d8 + ${spellMod}`, "force"] // ["Formula: this can be like '${dice}d8' (include the backticks) to use the spell scaling", "acid, bludgeoning, cold, fire, force, lightning, necrotic, piercing, poison, psychic, radiant, slashing, thunder, healing, temphp"]
+						[`${dice}d8 + ${spellMod}`, "force"] // ["Formula: this can be like '${dice}d8' (include the backticks) to use the spell level", "acid, bludgeoning, cold, fire, force, lightning, necrotic, piercing, poison, psychic, radiant, slashing, thunder, healing, temphp"]
 						],
 					},
 				}
@@ -51,12 +51,12 @@ const updates = {
 		}
 	}
 };
-const callbacks = {};
-const options = {controllingActor: actor};
+const callbacks = {}; // Still have to figure this one out
+const options = {controllingActor: actor}; // This will minimize the character sheet while you are placing the token
 
 if(args[0] === "on") {
 	await warpgate.spawn(summonType, updates, callbacks, options);
 }
 else if(args[0] === "off") {
-	await warpgate.dismiss(); //Still have to figure this one out
+	await warpgate.dismiss(); // Still have to figure this one out
 }
