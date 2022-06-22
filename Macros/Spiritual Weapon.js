22581b7266b2f5608c089d5ddc2fa5307dcbd203
HavocsCall <49051014+HavocsCall@users.noreply.github.com>
2022-06-22 18:57:39 -0400

    Update Spiritual Weapon.js

--- Macros/Spiritual Weapon.js
+++ Macros/Spiritual Weapon.js
@@ -21,25 +21,7 @@ const updates = { // Updates to token on summon
 	embedded: {
 		Item: { // Define the item that you want in the summoned token's inventory
 			"Spiritual Weapon Attack": { // String
-				type: "feat", // feat, spell, weapon
-				img: "icons/skills/melee/strike-blade-knife-blue-red.webp", // Example: "icons/magic/holy/projectiles-blades-salvo-yellow.webp"
 				data: {
-					description: {
-						value: "You can make a melee spell attack against a creature within 5 feet of the weapon. On a hit, the target takes force damage equal to 1d8 + your spellcasting ability modifier.<br>When you cast this spell using a spell slot of 3rd level or higher, the damage increases by 1d8 for every two slot levels above 2nd.", // String
-					},
-					activation: {
-						type: "bonus", // action, bonus, crew, day, hour, lair, legendary, minute, none, reaction, special, reactiondamage, reactionmanual
-						cost: 1, // Numeric
-					},
-					target: {
-						value: 1, // Numeric
-						type: "creature" // ally, cone, creature, cube, cylinder, enemy, line, none, object, radius, self, space, sphere, square, wall
-					},
-					range: {
-						value: 5, // Numeric
-						units: "ft" // self, touch, spec, any, ft, mi, m, km
-					},
-					actionType: "msak", // mwak, rwak, msak, rsak, save, heal, abil, util, other
 					attackBonus: `- @mod - @prof + ${spellAttack}`, // Formula
 					damage: {
 						parts: [
